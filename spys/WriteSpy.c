e1a1bdc7b2371ade2045cbe6321b63535b87b008
dylanR33 <ruiz.dylaneduardo@gmail.com>
2025-02-19 11:29:49 -0800

    Added a 8 bit write command.

--- spys/WriteSpy.c
+++ spys/WriteSpy.c
@@ -2,11 +2,13 @@
 
 static uint16_t lastWrite16 = 0;
 static uint32_t lastWrite32 = 0;
+static uint8_t  lastWrite8  = 0;
 
 void WriteSpy_Create()
 {
     lastWrite16 = 0;
     lastWrite32 = 0;
+    lastWrite8  = 0;
 }
 
 uint16_t WriteSpy_GetLastWrite16()
@@ -19,6 +21,11 @@ uint32_t WriteSpy_GetLastWrite32()
     return lastWrite32;
 }
 
+uint8_t WriteSpy_GetLastWrite8()
+{
+    return lastWrite8;
+}
+
 void WriteSpy_Write16( uint16_t cmd )
 {
     lastWrite16 = cmd;
@@ -28,3 +35,9 @@ void WriteSpy_Write32( uint32_t cmd )
 {
     lastWrite32 = cmd;
 }
+
+void WriteSpy_Write8( uint8_t cmd )
+{
+    lastWrite8 = cmd;
+}
+
--- spys/WriteSpy.h
+++ spys/WriteSpy.h
@@ -9,9 +9,13 @@ uint16_t WriteSpy_GetLastWrite16();
 
 uint32_t WriteSpy_GetLastWrite32();
 
+uint8_t WriteSpy_GetLastWrite8();
+
 void WriteSpy_Write16( uint16_t cmd );
 
 void WriteSpy_Write32( uint32_t cmd );
+
+void WriteSpy_Write8( uint8_t cmd );
 
 #endif
 
